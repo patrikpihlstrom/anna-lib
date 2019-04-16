7774c5b6d1d6453d88a9811ea027271d32398c1a
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-04-16 22:25:27 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
 	name='anna_lib',
-	version='0.0.5',
+	version='0.0.6',
 	author='Patrik Pihlstrom',
 	author_email='patrik.pihlstrom@gmail.com',
 	url='https://github.com/patrikpihlstrom/anna-lib',
 	description='selenium interface',
 	long_description=description,
 	long_description_content_type='text/markdown',
-	packages=['anna_lib'],
+	packages=['anna_lib', 'anna_lib.task'],
 	install_requires=[
 		'selenium'
 	]
