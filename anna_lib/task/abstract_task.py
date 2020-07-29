c5802ee47c5c2eef90d0f67aa29af631b656057c
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2020-07-29 20:16:45 +0200

    EventExecutor

--- anna_lib/task/abstract_task.py
+++ anna_lib/task/abstract_task.py
@@ -1,71 +1,42 @@
 import abc
 
-from selenium.webdriver.remote.webdriver import WebDriver
-
 from anna_lib.selenium import assertions
+from anna_lib.selenium.event_executor import EventExecutor
 
 
-class AbstractTask(object):
-	driver: WebDriver
+class AbstractTask(EventExecutor):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.result = []
+        self.passed = False
 
-	def __init__(self, driver):
-		self.driver = driver
-		self.result = []
-		self.passed = False
-		self.required = True
+    def execute(self) -> None:
+        self.before_execute()
+        self.__execute__()
+        self.after_execute()
+        self.passed = all(r['passed'] is True for r in self.result)
 
-	def execute(self) -> None:
-		self.before_execute()
-		self.__execute__()
-		self.after_execute()
-		if self.required:
-			self.passed = not any(not r for r in self.result)
-		else:
-			self.passed = True
+    @abc.abstractmethod
+    def before_execute(self) -> None:
+        pass
 
-	@abc.abstractmethod
-	def before_execute(self) -> None:
-		pass
+    @abc.abstractmethod
+    def __execute__(self) -> None:
+        pass
 
-	@abc.abstractmethod
-	def __execute__(self) -> None:
-		pass
+    @abc.abstractmethod
+    def after_execute(self) -> None:
+        pass
 
-	@abc.abstractmethod
-	def after_execute(self) -> None:
-		pass
-
-	def assert_element_exists(self, target: str) -> None:
-		try:
-			self.result.append({'assertion': 'element_exists', 'passed': assertions.element_exists(driver=self.driver, target=target)})
-		except TypeError as e:
-			self.result.append(
-				{
-					'assertion': 'element_exists',
-					'passed': False,
-					'log': str(e)
-				})
-
-	def assert_url_equals(self, expected: str) -> None:
-		try:
-			self.result.append({'assertion': 'url_equals',
-			                    'passed': assertions.url_equals(driver=self.driver, expected=expected)})
-		except ValueError as e:
-			self.result.append(
-				{
-					'assertion': 'url_equals',
-					'passed': False,
-					'log': str(e)
-				})
-
-	def assert_in_url(self, part: str) -> None:
-		try:
-			self.result.append({'assertion': 'in_url',
-			                    'passed': assertions.in_url(driver=self.driver, part=part)})
-		except ValueError as e:
-			self.result.append(
-				{
-					'assertion': 'in_url',
-					'passed': False,
-					'log': str(e)
-				})
+    def _assert(self, assertion: str, param: str) -> None:
+        try:
+            method = getattr(assertions, assertion)
+            self.result.append(
+                {'assertion': assertion, 'passed': method(self.driver, param) is True})
+        except TypeError as e:
+            self.result.append(
+                {
+                    'assertion': assertion,
+                    'passed': False,
+                    'log': str(e)
+                })
